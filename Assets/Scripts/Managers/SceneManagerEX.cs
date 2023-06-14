11b7c3772d7e600f0024ce404314fe145e02dd16
dMinsz <dsminzks@gmail.com>
2023-06-14 19:04:32 +0900

    feat: Feat SceneManger

--- Assets/Scripts/Managers/SceneManagerEX.cs
+++ Assets/Scripts/Managers/SceneManagerEX.cs
@@ -5,5 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerEX : MonoBehaviour
 {
-  
+    public BaseScene CurrentScene { get { return GameObject.FindObjectOfType<BaseScene>(); } }
+
+    public BaseScene nextScene;
+
+    string GetSceneName(BaseScene type)
+    {
+        string name = type.name; // C#¿« Reflection. Scene enum¿« 
+        return name;
+    }
+
+    public void LoadScene(BaseScene type)
+    {
+        nextScene = type;
+        SceneManager.LoadScene(GetSceneName(type));
+    }
+
+    public AsyncOperation LoadSceneAsync(BaseScene nextScene)
+    {
+        return SceneManager.LoadSceneAsync(GetSceneName(nextScene));
+    }
+
+
+    public void Clear()
+    {
+        CurrentScene.Clear();
+    }
 }
---
+++ Assets/Scripts/Scenes/BaseScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseScene : MonoBehaviour
+{
+    //public SceneDefine.Scene SceneType { get; protected set; } = SceneDefine.Scene.Unknown; // 디폴트로 Unknow 이라고 초기화
+
+    void Awake()
+    {
+        Init();
+    }
+
+    protected virtual void Init()
+    {
+    }
+
+    public abstract void Clear();
+}
