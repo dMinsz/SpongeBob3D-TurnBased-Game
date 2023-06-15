6bb7a853b7b2966ba2120fccaf4f6e9b51c00d78
Hyeonuk Ryu <rpoiu0320@gmail.com>
2023-06-15 16:18:33 +0900

    feat : Enviroments Layer, InteractAdaptor
    상호작용 대강 구현, 맵 레이어 적용

---
+++ Assets/Scripts/WorldSceneScript/WorldEnemy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldEnemy : MonoBehaviour
+{
+    public void Interactor()
+    {
+        Debug.Log("LoadingSceneChange");
+    }
+}
