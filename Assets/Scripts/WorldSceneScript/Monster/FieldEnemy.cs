ae44a47f5ab286187c2fd0411e1d1487f2fa0f7f
Hyeonuk Ryu <rpoiu0320@gmail.com>
2023-06-15 19:14:44 +0900

    feat : Player, FieldEnemy Prefab
    프리팹 가제작

--- Assets/Scripts/WorldSceneScript/Monster/FieldEnemy.cs
+++ Assets/Scripts/WorldSceneScript/Monster/FieldEnemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using EnemyState;
 using Unity.IO.LowLevel.Unsafe;
+using Unity.Burst.Intrinsics;
 
 public class FieldEnemy : MonoBehaviour
 {
@@ -50,6 +51,8 @@ namespace EnemyState
     {
         private FieldEnemy fieldEnemy;
 
+        private float idleTime;
+
         public IdleState(FieldEnemy fieldEnemy)
         {
             this.fieldEnemy = fieldEnemy;
@@ -57,12 +60,19 @@ namespace EnemyState
 
         public override void Enter()
         {
-            
+            Debug.Log("대기진입");
+            idleTime = 0;
         }
 
         public override void Update()
         {
+            idleTime += Time.deltaTime;
 
+            if (idleTime > 5)
+            {
+                idleTime = 0;
+                fieldEnemy.ChangeState(StateEnemy.Patrol);
+            }
         }
 
         public override void Exit()
